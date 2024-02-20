ef19a8cb69f2df35a7436fc82fd654ed4c782efc
csolly <cjaysolly@gmail.com>
2024-02-20 13:19:44 -0500

    Lord vaders pod has been made

--- OoP.py
+++ OoP.py
@@ -3,6 +3,7 @@
 # AnakinsPod that inherits Podracer also contains special method called boost... mulitply max_speed by 2
 # SebulbasPod inherits PodRacer as well but also has a special method flame_jet, update the condition of another podracer to trashed
 
+# main parent class to which the children will inherit
 class PodRacer:
     def __init__(self,max_speed,condition,price):
         self.max_speed = max_speed
@@ -11,3 +12,14 @@ class PodRacer:
 
     def repair(self):
         self.condition = "Like it was brand new"
+
+# Anakin is lame name.. we all know he is and always shall be Lord Vader
+class VadersPod:
+    # dont remember what the none does here on 19... just gonna leave it
+    def __init__(self,max_speed,condition,price) -> None:
+        # super init brings down the settings of self.max_speed = max_speed etc etc
+        super.init(max_speed,condition,price)
+
+    def boost(self):
+        self.max_speed *= 2
+        print("Use the force Lord Vader")
